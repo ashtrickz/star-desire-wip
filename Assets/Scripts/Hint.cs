3e2cdda23e78c61cc943e0dde233fe859f43db86
ashtrickz <ashshvern@gmail.com>
2022-06-22 23:06:16 +0300

    Fixed Hint, Added Hint blink

--- Assets/Scripts/Hint.cs
+++ Assets/Scripts/Hint.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
-using UnityEngine.UI;
 
 public class Hint : MonoBehaviour
 {
     private PlayerMovement playerMovement;
     
-    void Start() => playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
-
+    void Start() =>
+    playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+    
     void Update()
     {
         if (playerMovement.allowFlight)
