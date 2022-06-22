2fcb3a3e23c31c8dac1e48044b178a03da52e6f9
ashtrickz <ashshvern@gmail.com>
2022-06-22 14:36:49 +0300

    Added Weapon Change Button instead of MWheel Scroll

--- Assets/Scripts/WeaponChanger.cs
+++ Assets/Scripts/WeaponChanger.cs
@@ -7,32 +7,8 @@ public class WeaponChanger : MonoBehaviour
 {
     [SerializeField] int selectedWeapon;
 
-    void Start()
-    {
-        SelectWeapon();
-    }
-
-    private void Update()
-    {
-        int previousWeapon = selectedWeapon;
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
-        {
-            if (selectedWeapon >= transform.childCount - 1)
-                selectedWeapon = 0;
-            else 
-                selectedWeapon++;
-        }        
-        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
-        {
-            if (selectedWeapon <= 0)
-                selectedWeapon = transform.childCount - 1;
-            else
-                selectedWeapon--;
-        }
-        if (previousWeapon != selectedWeapon)
-            SelectWeapon();
-    }
-
+    void Start() => SelectWeapon();
+    
     void SelectWeapon()
     {
         int i = 0;
@@ -40,10 +16,18 @@ public class WeaponChanger : MonoBehaviour
         {
             if (i == selectedWeapon)
                 weapon.gameObject.SetActive(true);
-            else
-                weapon.gameObject.SetActive(false);
+            else weapon.gameObject.SetActive(false);
             i++;
         }
     }
 
+    public void ChangeWeapon()
+    {
+        int previousWeapon = selectedWeapon;
+        if (selectedWeapon >= transform.childCount - 1)
+            selectedWeapon = 0;
+        else selectedWeapon++;
+        if (previousWeapon != selectedWeapon)
+            SelectWeapon();
+    }
 }
